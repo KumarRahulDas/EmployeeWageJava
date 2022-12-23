4f7d61bdf50ceda340eaeef1f2f7017da2f74cd1
KumarRahulDas <dasrahulbxrepa7061503554@gmail.com>
2022-12-23 00:58:49 -0800

    Commited Succesfully

--- src/com/bridglabz/employeeWage/EmployeeWage.java
+++ src/com/bridglabz/employeeWage/EmployeeWage.java
@@ -1,7 +1,21 @@
 package com.bridglabz.employeeWage;
-
+import java.util.Random;
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
+        Random random = new Random();
+        int attendance = random.nextInt(2);
+
+        int WAGEPERHOUR = 20;
+        int FULLDAYHOURS = 8;
+        int WAGEPERDAY = 0;
+
+        if (attendance == 1) {
+            System.out.println("Employee Present..");
+            WAGEPERDAY = WAGEPERHOUR * FULLDAYHOURS;
+        } else
+            System.out.println("Employee Absent");
+
+        System.out.println("Employee Wage Per Day: " + WAGEPERDAY);
     }
 }
