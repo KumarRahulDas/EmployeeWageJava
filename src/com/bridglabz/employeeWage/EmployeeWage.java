558f0698f907a3efb883a568a538e37b5502bd3c
KumarRahulDas <dasrahulbxrepa7061503554@gmail.com>
2022-12-23 08:07:58 -0800

    Newly Updated

--- src/com/bridglabz/employeeWage/EmployeeWage.java
+++ src/com/bridglabz/employeeWage/EmployeeWage.java
@@ -1,11 +1,14 @@
 package com.bridglabz.employeeWage;
+
 import java.util.Random;
+
 public class EmployeeWage {
+
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
+
         Random random = new Random();
-<<<<<<< HEAD
-        int attendance = random.nextInt();
+        int attendance = random.nextInt(2);
 
         int WAGEPERHOUR = 20;
         int FULLDAYHOURS = 8;
@@ -17,26 +20,8 @@ public class EmployeeWage {
         } else
             System.out.println("Employee Absent");
 
-=======
-        int attendance = random.nextInt(3);
-
-        int WAGEPERHOUR = 20;
-        int FULLDAYHOURS = 8;
-        int PARTTIMEHOURS = 4;
-        int WAGEPERDAY = 0;
-
-        switch (attendance) {
-            case 0:
-                System.out.println("Employee Absent.");
-                break;
-            case 1:
-                WAGEPERDAY = PARTTIMEHOURS * WAGEPERHOUR;
-                break;
-            default:
-                WAGEPERDAY = FULLDAYHOURS * WAGEPERHOUR;
-
-        }
->>>>>>> 0876ed613bbf86270e447df6e50d48392985b9cf
         System.out.println("Employee Wage Per Day: " + WAGEPERDAY);
+
     }
+
 }
